572a61ff6f26abb41aeba5735c81d564c9df7e82
Haneefah55 <olasupoomotayo@gmail.com>
2026-06-23 12:34:15 +0100

    fix import issues

---
+++ endpoints/index.js
@@ -0,0 +1,9 @@
+const createCreatorCard = require('./creator-card/create-card');
+const retrieveCreatorCard = require('./creator-card/retrieve-card');
+const deleteCreatorCard = require('./creator-card/delete-card');
+
+module.exports = {
+  createCreatorCard,
+  retrieveCreatorCard,
+  deleteCreatorCard,
+};
