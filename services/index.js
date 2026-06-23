cbbed1506bf51075afad353da852689a9f093e35
Haneefah55 <olasupoomotayo@gmail.com>
2026-06-23 12:38:25 +0100

    fix imporr error in services module

--- services/index.js
+++ services/index.js
@@ -1,6 +1,6 @@
-const createCreatorCard = require('./creator-cards/create-card');
-const retrieveCreatorCard = require('./creator-cards/retrieve-card');
-const deleteCreatorCard = require('./creator-cards/delete-card');
+const createCreatorCard = require('./creator-card/create-card');
+const retrieveCreatorCard = require('./creator-card/retrieve-card');
+const deleteCreatorCard = require('./creator-card/delete-card');
 
 module.exports = {
   createCreatorCard,
